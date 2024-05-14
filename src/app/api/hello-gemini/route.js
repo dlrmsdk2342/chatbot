47b0715f7faaf2d30517ddc67359ce688dd7223e
dlrmsdk2342 <dlrmsdk2342@gmail.com>
2024-05-14 12:18:21 +0900

    vercel

--- src/app/api/hello-gemini/route.js
+++ src/app/api/hello-gemini/route.js
@@ -34,7 +34,7 @@ export async function GET() {
   //   const msg = "오늘 신나는 일이 있었어. 한 번 들어볼래?";
   //   const msg = "내가 무슨 말을 하고 있었지?";
 
-  const result = await chat.sendMessage(msg);
+  const result = await chat.sendMessage("");
   const response = await result.response;
   const text = response.text();
   //   console.log(response.candidates[0].content);
